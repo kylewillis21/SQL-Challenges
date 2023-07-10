c03e5351ea9e4f88f0ee95c56a4e3cbd4b3fecda
Kyle Willis <kw586717@ohio.edu>
2023-07-10 16:13:42 -0400

    Added link to challenges

--- SteelData/challenge2/queries.sql
+++ SteelData/challenge2/queries.sql
@@ -1,3 +1,4 @@
+-- Challenge found at https://steeldata.org.uk/sql2.html
 -- # 1 What are the names of the players whose salary is greater than 100,000?
 SELECT player_name, salary FROM Players
 WHERE salary > 100000
